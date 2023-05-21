25acf3d2d11f648f57b3f48c383ccfda7cb55561
TKMann <tiffany.mann@radicalsystems.com.au>
2023-05-21 22:38:06 +0930

    Database no longer working, but photo is

--- app/src/main/java/com/tmannapps/truck_app_blank/MainActivity.java
+++ app/src/main/java/com/tmannapps/truck_app_blank/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
 
     DatabaseHelper db;
     BroadcastReceiver myBroadcastReceiver;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/tmannapps/truck_app_blank/SignUpActivity.java
+++ app/src/main/java/com/tmannapps/truck_app_blank/SignUpActivity.java
@@ -1,34 +1,45 @@
 package com.tmannapps.truck_app_blank;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.FileProvider;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.tmannapps.truck_app_blank.data.DatabaseHelper;
+import com.tmannapps.truck_app_blank.databinding.ActivitySignUpBinding;
 import com.tmannapps.truck_app_blank.model.User;
+import com.tmannapps.truck_app_blank.util.Util;
 
+import java.io.File;
+//camera setup and permissions from youtube tutorial- www.youtube.com/watch?v=9XSlbZN1yFg
 public class SignUpActivity extends AppCompatActivity {
     DatabaseHelper db;
-    TextView textView;
-
+    ActivitySignUpBinding signUpBinding;
+    ActivityResultLauncher<Uri> takePictureLauncher;
+    Uri imageUri;
+    private static final int CAMERA_PERMISSION_CODE = 1;
     BroadcastReceiver myBroadcastReceiver = new BroadcastReceiver(){
         @Override
         public void onReceive(Context context, Intent intent){
             Bundle bundle = intent.getExtras();
             String value =  bundle.getString("return_value");
-            textView = findViewById(R.id.textView);
-            textView.setText(value);
-            Toast.makeText(SignUpActivity.this, "value "  + value +" ToastText in onReceive from Broadcast receiver", Toast.LENGTH_SHORT).show();
-
+            //Toast.makeText(SignUpActivity.this, "value "  + value +" ToastText in onReceive from Broadcast receiver", Toast.LENGTH_SHORT).show();
         }
     };
 
@@ -38,56 +49,115 @@ public class SignUpActivity extends AppCompatActivity {
         super.onResume();
         registerReceiver(myBroadcastReceiver, new IntentFilter("com.tmannapps.truck_app_blank.MyIntentService"));
     }
+
+    private Uri createUri(){
+        File imageFile = new File(getApplicationContext().getFilesDir(), "camera_photo.jpg");
+        return FileProvider.getUriForFile(
+                getApplicationContext(),
+                "com.tmannapps.truck_app_blank.fileProvider",
+                imageFile
+        );
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
         EditText sUsernameEditText = findViewById(R.id.sUsernameEditText);
+
         EditText sPasswordEditText = findViewById(R.id.sPasswrodEditText);
         EditText fullNameEditText = findViewById(R.id.fullNameEditText);
         EditText phoneEditText = findViewById(R.id.phoneEditTextNumber);
         EditText sConfirmPasswordEditText = findViewById(R.id.sConfirmPasswordEditText);
         Button saveButton = findViewById(R.id.saveButton);
-        FloatingActionButton takePictureButton = findViewById(R.id.floatingActionButton);
+        signUpBinding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(signUpBinding.getRoot());
 
         db = new DatabaseHelper(this);
 
-        saveButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String username = sUsernameEditText.getText().toString();
-                String password = sPasswordEditText.getText().toString();
-                String fullname = fullNameEditText.getText().toString();
-                int phone = Integer.parseInt(phoneEditText.getText().toString());
-                String confPassword = sConfirmPasswordEditText.getText().toString();
-
-                if (password.equals(confPassword))
-                {
-                    long result = db.insertUser(new User(username, password, fullname, phone));
-                    if (result > 0)
-                    {
-                        Toast.makeText(SignUpActivity.this, "Registration successful", Toast.LENGTH_SHORT).show();
-                    }
-                    else
-                    {
-                        Toast.makeText(SignUpActivity.this, "Registration failed. Try again.", Toast.LENGTH_SHORT).show();
-                    }
-                }
-                else
-                {
-                    Toast.makeText(SignUpActivity.this, "Passwords do not match.", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-
-        takePictureButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        if (SignUpActivity.this.getPackageManager()
+                .hasSystemFeature(PackageManager.FEATURE_CAMERA_FRONT)) {
+            //do the camera part here
+            imageUri = createUri();
+            registerPictureLauncher();
+            signUpBinding.floatingActionButton.setOnClickListener(view -> {
+                checkCameraPermissionAndOpenCamera();
                 Intent intent = new Intent(SignUpActivity.this, MyIntentService.class);
                 intent.putExtra("value", "value from Sign Up Page");
                 startService(intent);
+            });
+        } else {
+            //have user upload from file - TODO
+            Toast.makeText(SignUpActivity.this, ("Looks like you don't have a camera"), Toast.LENGTH_SHORT).show();
+        }
+
+
+        saveButton.setOnClickListener(v -> {
+
+            String username = sUsernameEditText.getText().toString();
+            String password = sPasswordEditText.getText().toString();
+            String fullname = fullNameEditText.getText().toString();
+            int phone = Integer.parseInt(phoneEditText.getText().toString());
+            String confPassword = sConfirmPasswordEditText.getText().toString();
+
+            if (password.equals(confPassword))
+            {
+                long result = db.insertUser(new User(username, password, fullname, phone));
+                if (result > 0)
+                {
+                    Toast.makeText(SignUpActivity.this, "Registration successful", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Toast.makeText(SignUpActivity.this, "Registration failed. Try again.", Toast.LENGTH_SHORT).show();
+                }
+            }
+            else
+            {
+                Toast.makeText(SignUpActivity.this, "Passwords do not match.", Toast.LENGTH_SHORT).show();
             }
         });
+
+
+    }
+    private void registerPictureLauncher() {
+        takePictureLauncher = registerForActivityResult(
+                new ActivityResultContracts.TakePicture(),
+                new ActivityResultCallback<Boolean>() {
+                    @Override
+                    public void onActivityResult(Boolean result) {
+                        try {
+                            if (result) {
+                                signUpBinding.imageView2.setImageURI(null);
+                                signUpBinding.imageView2.setImageURI(imageUri);
+                            }
+                        } catch (Exception e) {
+                            e.getStackTrace();
+                        }
+                    }
+                }
+        );
+    }
+    private void checkCameraPermissionAndOpenCamera() {
+        if (ActivityCompat.checkSelfPermission(SignUpActivity.this,
+                android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(SignUpActivity.this,
+                    new String[]{Manifest.permission.CAMERA}, CAMERA_PERMISSION_CODE);
+        } else {
+            takePictureLauncher.launch(imageUri);
+        }
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == CAMERA_PERMISSION_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                takePictureLauncher.launch(imageUri);
+
+            } else {
+                Toast.makeText(SignUpActivity.this, "Camera permission denied, please allow permissions", Toast.LENGTH_SHORT).show();
+
+            }
+        }
+    }
 }
